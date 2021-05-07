c73b609954e0771e6f1599fe61368501fe608774
mohdarshad1 <mdarshad181@gmail.com>
2021-05-07 17:53:39 +0530

    [Mohd Arshad] : Add Best Rated Hotel for Reward Customer

--- lib/src/main/java/com/HotelReservationSystem/HotelReservation.java
+++ lib/src/main/java/com/HotelReservationSystem/HotelReservation.java
@@ -7,6 +7,8 @@ import java.time.temporal.ChronoField;
 import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HotelReservation {
 	private static Map<String, Hotel> hotelMap;
@@ -20,21 +22,23 @@ public class HotelReservation {
 		hotelMap.put(name, hotelObject);
 		return true;
 	}
-	
+
 	public boolean addHotel(String name, int regWeekdayRate, int regWeekendRate) {
 		Hotel hotelObject = new Hotel(name, regWeekdayRate, regWeekendRate);
 		hotelMap.put(name, hotelObject);
 		return true;
 	}
-	
+
 	public boolean addHotel(String name, int regWeekdayRate, int regWeekendRate, int hotelRating) {
 		Hotel hotelObject = new Hotel(name, regWeekdayRate, regWeekendRate, hotelRating);
 		hotelMap.put(name, hotelObject);
 		return true;
 	}
-	
-	public boolean addHotel(String name, int regWeekdayRate, int regWeekendRate, int hotelRating, int rewWeekdayRate, int rewWeekendRate) {
-		Hotel hotelObject = new Hotel(name, regWeekdayRate, regWeekendRate, hotelRating, rewWeekdayRate, rewWeekendRate);
+
+	public boolean addHotel(String name, int regWeekdayRate, int regWeekendRate, int hotelRating, int rewWeekdayRate,
+			int rewWeekendRate) {
+		Hotel hotelObject = new Hotel(name, regWeekdayRate, regWeekendRate, hotelRating, rewWeekdayRate,
+				rewWeekendRate);
 		hotelMap.put(name, hotelObject);
 		return true;
 	}
@@ -42,33 +46,33 @@ public class HotelReservation {
 	public void printHotels() {
 		for (Map.Entry<String, Hotel> entry : hotelMap.entrySet()) {
 			System.out.println("Hotel Name : " + entry.getKey());
-			System.out.println("Rate on weekdays for regular customers : " + entry.getValue().getRegWeekdayRate());
-			System.out.println("Rate on weekends for regular customers : " + entry.getValue().getRegWeekendRate());
+			System.out.println("Rate on weekdays for Regular Customers : " + entry.getValue().getRegWeekdayRate());
+			System.out.println("Rate on weekends for Regular Customers : " + entry.getValue().getRegWeekendRate());
 			System.out.println("Hotel Rating : " + entry.getValue().getHotelRating());
-			System.out.println("Rate on weekdays for reward customers : " + entry.getValue().getRewWeekdayRate());
-			System.out.println("Rate on weekdays for reward customers : " + entry.getValue().getRewWeekdayRate());
+			System.out.println("Rate on weekdays for Reward Customers : " + entry.getValue().getRewWeekdayRate());
+			System.out.println("Rate on weekdays for Reward Customers : " + entry.getValue().getRewWeekdayRate());
 			System.out.println();
 		}
 	}
-	
-	public Boolean findCheapestHotel(String fromDate, String toDate) {
-		Map<Integer, ArrayList<Hotel>> rentMap = createRentMap(fromDate, toDate);
-		int minimumRent = Integer.MAX_VALUE;						
+
+	public Boolean findCheapestHotel(String customerType, String fromDate, String toDate) {
+		Map<Integer, ArrayList<Hotel>> rentMap = createRentMap(customerType, fromDate, toDate);
+		int minimumRent = Integer.MAX_VALUE; // Assigns max possible value
 		for (Map.Entry<Integer, ArrayList<Hotel>> entry : rentMap.entrySet()) {
 			if (entry.getKey() < minimumRent) {
 				minimumRent = entry.getKey();
 			}
 		}
-		System.out.println("Cheapest Hotel for you is ");
+		System.out.println("Cheapest Hotel for you is : ");
 		for (Hotel hotel : rentMap.get(minimumRent)) {
-			System.out.print(hotel.getHotelName() + "  "); 			
+			System.out.print(hotel.getHotelName() + "  "); 
 		}
-		System.out.println("Total Rent : " + minimumRent);			
+		System.out.println("\nTotal Rent : " + minimumRent); 
 		return true;
 	}
 
-	public boolean cheapestBestRatedHotel(String fromDate, String toDate) {
-		Map<Integer, ArrayList<Hotel>> rentMap = createRentMap(fromDate, toDate);
+	public boolean cheapestBestRatedHotel(String customerType, String fromDate, String toDate) {
+		Map<Integer, ArrayList<Hotel>> rentMap = createRentMap(customerType, fromDate, toDate);
 		int minimumRent = Integer.MAX_VALUE;
 		for (Map.Entry<Integer, ArrayList<Hotel>> entry : rentMap.entrySet()) {
 			if (entry.getKey() < minimumRent)
@@ -83,11 +87,11 @@ public class HotelReservation {
 				rating = hotel.getHotelRating();
 			}
 		}
-		System.out.println("Cheapest Hotel for you is : " + bestRatedCheapestHotel + " with Rating  " + rating + " Total Rent : "
-				+ minimumRent + "\n");
+		System.out.println("Cheapest Hotel for you is : " + bestRatedCheapestHotel + " with Rating  " + rating
+				+ " Total Rent : " + minimumRent + "\n");
 		return true;
 	}
-	
+
 	public boolean findBestRatedHotelForGivenDates(String fromDate, String toDate) {
 		int rating = 0;
 		int rent = 0;
@@ -103,19 +107,24 @@ public class HotelReservation {
 		System.out.println("Best Rated Hotel : " + bestRatedHotel + ", Rent : " + rent);
 		return true;
 	}
-	
-	public static Map<Integer, ArrayList<Hotel>> createRentMap(String fromDate, String toDate) {
+
+	public static Map<Integer, ArrayList<Hotel>> createRentMap(String customerType, String fromDate, String toDate) {
 		HashMap<Integer, ArrayList<Hotel>> rentMap = new HashMap<>();
 		int days[] = numberOfDays(fromDate, toDate);
+		int totalRent = 0;
 		for (Map.Entry<String, Hotel> entry : hotelMap.entrySet()) {
-			int totalRent = calculateRent(fromDate, toDate, entry.getValue().getRegWeekdayRate(),
-					entry.getValue().getRegWeekendRate());
-			
+			if (customerType.equalsIgnoreCase("Regular")) {
+				totalRent = calculateRent(fromDate, toDate, entry.getValue().getRegWeekdayRate(),
+						entry.getValue().getRegWeekendRate());
+			} else if (customerType.equalsIgnoreCase("Reward")) {
+				totalRent = calculateRent(fromDate, toDate, entry.getValue().getRewWeekdayRate(),
+						entry.getValue().getRewWeekendRate());
+			}
 			rentMap.computeIfAbsent(totalRent, key -> new ArrayList<>()).add(entry.getValue());
 		}
 		return rentMap;
 	}
-	
+
 	public static int calculateRent(String fromDate, String toDate, int weekdayRate, int weekendRate) {
 		int[] numOfDays = numberOfDays(fromDate, toDate);
 		int weekdayRent = weekdayRate * numOfDays[0];
@@ -126,26 +135,26 @@ public class HotelReservation {
 
 	public static int[] numberOfDays(String fromDate, String toDate) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
-		
-		LocalDate from = LocalDate.parse(fromDate, formatter);   
-		LocalDate to = LocalDate.parse(toDate, formatter);		 
+
+		LocalDate from = LocalDate.parse(fromDate, formatter); // convert String to LocalDate
+		LocalDate to = LocalDate.parse(toDate, formatter); // convert String to LocalDate
 		int numWeekdays = 0;
 		int numWeekendDays = 0;
 		int days[];
 		days = new int[2];
-		
+
 		for (LocalDate date = from; date.isBefore(to.plusDays(1)); date = date.plusDays(1)) {
 			DayOfWeek day = DayOfWeek.of(date.get(ChronoField.DAY_OF_WEEK));
-			switch(day) {
-			case SATURDAY :
+			switch (day) {
+			case SATURDAY:
 				numWeekendDays++;
 				break;
-				
-			case SUNDAY :
+
+			case SUNDAY:
 				numWeekendDays++;
 				break;
-				
-			default :
+
+			default:
 				numWeekdays++;
 				break;
 			}
@@ -153,6 +162,18 @@ public class HotelReservation {
 		days[0] = numWeekdays;
 		days[1] = numWeekendDays;
 		return days;
+	}
+
+	public void validateInputs(String customerType, String fromDate, String toDate) throws InvalidEntryException {
+		String regex = "^[0-9]{2}[ ][A-Za-z]{3}[ ][0-9]{4}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcherFrom = pattern.matcher(fromDate);
+		Matcher matcherTo = pattern.matcher(toDate);
+		if (!matcherFrom.find() || !matcherTo.find() || !customerType.equalsIgnoreCase("Regular")
+				|| !customerType.equalsIgnoreCase("Reward")) {
+			throw new InvalidEntryException("Invalid input, Please enter a valid input");
+		}
+		return;
 	}
 
 	public static void main(String[] args) {
--- lib/src/test/java/com/HotelReservationSystem/HotelReservationTest.java
+++ lib/src/test/java/com/HotelReservationSystem/HotelReservationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
- public class HotelReservationTest {
+public class HotelReservationTest {
 
 	@Test
 	public void whenNewHotelAdded_shouldReturnTrue() {
@@ -13,40 +13,38 @@ import org.junit.Test;
 		HotelReservation hotelReservationObject = new HotelReservation();
 		assertTrue(hotelReservationObject.addHotel("Lakewood", 110));
 		assertTrue(hotelReservationObject.addHotel("Bridgewood", 160));
-		assertTrue(hotelReservationObject.addHotel("Ridgewood", 220));
-
+		assertTrue(hotelReservationObject.addHotel("Ridgewood", 110));
 		hotelReservationObject.printHotels();
 	}
-	
+
 	@Test
 	public void whenFindCheapestHotelMethodCalled_shouldReturn_nameOfHotel() {
 		HotelReservation hotelReservation = new HotelReservation();
 		assertTrue(hotelReservation.addHotel("Lakewood", 110));
 		assertTrue(hotelReservation.addHotel("Bridgewood", 160));
 		assertTrue(hotelReservation.addHotel("Ridgewood", 220));
-		assertEquals("Lakewood", hotelReservation.findCheapestHotel("10Sep2020", "11Sep2020"));
+		assertTrue(hotelReservation.findCheapestHotel("Regular", "11Sep2020", "12Sep2020"));
 	}
-	
+
 	@Test
 	public void whenNewHotelAddedWithWeekend_shouldReturnTrue() {
 
 		HotelReservation hotelReservationObject = new HotelReservation();
 		assertTrue(hotelReservationObject.addHotel("Lakewood", 110, 90));
 		assertTrue(hotelReservationObject.addHotel("Bridgewood", 160, 60));
-		assertTrue(hotelReservationObject.addHotel("Ridgewood", 110, 150));
-
+		assertTrue(hotelReservationObject.addHotel("Ridgewood", 220, 150));
 		hotelReservationObject.printHotels();
 	}
-	
+
 	@Test
 	public void whenFindCheapestHotelIsCalled_shouldReturn_nameOfHotelWithCheapestRent() {
 		HotelReservation hotelReservation = new HotelReservation();
 		assertTrue(hotelReservation.addHotel("Lakewood", 110, 90));
 		assertTrue(hotelReservation.addHotel("Bridgewood", 160, 60));
 		assertTrue(hotelReservation.addHotel("Ridgewood", 220, 150));
-		assertTrue(hotelReservation.findCheapestHotel("11Sep2020", "12Sep2020"));
-		}
-	
+		assertTrue(hotelReservation.findCheapestHotel("Regular", "11Sep2020", "12Sep2020"));
+	}
+
 	@Test
 	public void whenNewHotelAddedWithRating_shouldReturnTrue() {
 
@@ -54,29 +52,27 @@ import org.junit.Test;
 		assertTrue(hotelReservationObject.addHotel("Lakewood", 110, 90, 3));
 		assertTrue(hotelReservationObject.addHotel("Bridgewood", 160, 60, 4));
 		assertTrue(hotelReservationObject.addHotel("Ridgewood", 220, 150, 5));
-
 		hotelReservationObject.printHotels();
-		
 	}
-	
+
 	@Test
 	public void whenCheapestBestRatedCalled_shouldReturn_bestRatedHotel() {
-		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel("Lakewood", 110, 90, 3);
-		hotelReservation.addHotel("Bridgewood", 150, 50, 4);
-		hotelReservation.addHotel("Ridgewood", 220, 150, 5);
-		assertTrue(hotelReservation.cheapestBestRatedHotel("11Sep2020", "12Sep2020"));
+		HotelReservation hotelReservationObject = new HotelReservation();
+		hotelReservationObject.addHotel("Lakewood", 110, 90, 3);
+		hotelReservationObject.addHotel("Bridgewood", 150, 50, 4);
+		hotelReservationObject.addHotel("Ridgewood", 220, 150, 5);
+		assertTrue(hotelReservationObject.cheapestBestRatedHotel("Regular", "11Sep2020", "12Sep2020"));
 	}
-	
+
 	@Test
 	public void whenFindBestRatedMethodCalled_shouldReturn_bestRatedHotel() {
-		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel("Lakewood", 110, 90, 3);
-		hotelReservation.addHotel("Bridgewood", 150, 50, 4);
-		hotelReservation.addHotel("Ridgewood", 220, 150, 5);
-		assertTrue(hotelReservation.findBestRatedHotelForGivenDates("11Sep2020", "12Sep2020"));
+		HotelReservation hotelReservationObject = new HotelReservation();
+		hotelReservationObject.addHotel("Lakewood", 110, 90, 3);
+		hotelReservationObject.addHotel("Bridgewood", 150, 50, 4);
+		hotelReservationObject.addHotel("Ridgewood", 220, 150, 5);
+		assertTrue(hotelReservationObject.findBestRatedHotelForGivenDates("11Sep2020", "12Sep2020"));
 	}
-	
+
 	@Test
 	public void whenAddedRatesForRewardCustomers_shouldAdd_hotelWithAllRates() {
 		HotelReservation hotelReservationObject = new HotelReservation();
@@ -84,5 +80,14 @@ import org.junit.Test;
 		hotelReservationObject.addHotel("Bridgewood", 150, 50, 4, 110, 50);
 		hotelReservationObject.addHotel("Ridgewood", 220, 150, 5, 100, 40);
 		hotelReservationObject.printHotels();
-		}
+	}
+
+	@Test
+	public void whenCheapestBestRatedCalledForRewardCustomers_shouldReturn_bestRatedHotel() {
+		HotelReservation hotelReservationObject = new HotelReservation();
+		hotelReservationObject.addHotel("Lakewood", 110, 90, 3);
+		hotelReservationObject.addHotel("Bridgewood", 150, 50, 4);
+		hotelReservationObject.addHotel("Ridgewood", 220, 150, 5);
+		assertTrue(hotelReservationObject.cheapestBestRatedHotel("Reward", "11Sep2020", "12Sep2020"));
+	}
 }
